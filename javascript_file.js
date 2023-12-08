d9b0e7f9f31fe8412a5285f115065240a736aa9f
Nakul <139488807+nakulgprbs@users.noreply.github.com>
2023-12-08 18:51:10 +0530

    Add files via upload

---
+++ javascript_file.js
@@ -0,0 +1,5 @@
+
+    // Function to show an alert on website opening
+    
+        alert("Welcome to notes by Mr. IITian, you can get all explaination of notes at youtube channel link at bottom, complete your syllbus easily and fast.");
+    
