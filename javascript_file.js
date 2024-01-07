ec289dc5d85738bea0b836a6365a4630d2081105
Nakul <nakulsingh9588@gmail.com>
2024-01-08 00:26:25 +0530

    github

--- javascript_file.js
+++ javascript_file.js
@@ -1,5 +1,5 @@
 
     // Function to show an alert on website opening
     
-        alert("Welcome to notes by Mr. IITian, you can get all explaination of notes at youtube channel link at bottom, complete your syllbus easily and fast.");
+        alert("Welcome to notes by Mr. IITian (Mr. Nakul singh), you can get all explaination of notes at youtube channel link at bottom, complete your syllbus easily and fast.");
     
