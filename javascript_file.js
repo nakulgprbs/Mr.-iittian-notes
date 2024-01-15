60dd79156187a2410995d6c9f8574596366e73bd
Nakul <nakulsingh9588@gmail.com>
2024-01-15 21:31:06 +0530

    physics notes added

--- javascript_file.js
+++ javascript_file.js
@@ -1,5 +1,5 @@
 
     // Function to show an alert on website opening
     
-        alert("Welcome to notes by Mr. IITian (Mr. Nakul singh), you can get all explaination of notes at youtube channel link at bottom, complete your syllbus easily and fast.");
+        alert("Welcome to notes by Mr. IITian (Mr. Nakul singh), all these note are hand witten by IIT students(me and my friends). you can get some explaination of notes at youtube channel link at bottom, complete your syllbus easily and fast.");
     
